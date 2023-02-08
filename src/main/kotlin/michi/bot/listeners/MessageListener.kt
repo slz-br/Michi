c2d65f136bc4fef1657f103fdf03631e02aee240
Slz <felipegoncalves2311@gmail.com>
2023-02-08 13:42:24 -0300

    Walter white command removed False message removed

--- src/main/kotlin/michi/bot/listeners/MessageListener.kt
+++ src/main/kotlin/michi/bot/listeners/MessageListener.kt
@@ -2,7 +2,6 @@ package michi.bot.listeners
 
 import michi.bot.commands.math.MathLogic
 import michi.bot.commands.math.checkAnswer
-import michi.bot.commands.walterwhite.sendRandomWalterWhiteImage
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import java.lang.NumberFormatException
@@ -17,13 +16,7 @@ class MessageListener: ListenerAdapter() {
         val msg = event.message.contentRaw
         val sender = event.author
 
-        // walter white
-        if (msg.contains("walter") || msg.contains("white")) {
-            sendRandomWalterWhiteImage(event)
-        }
-
         // math
-        // currently it throws concurrentModificationException
         for (instance in MathLogic.instances) {
 
             if (sender == instance.problemInstance.user && !sender.isBot && event.channel == instance.context.channel) {
