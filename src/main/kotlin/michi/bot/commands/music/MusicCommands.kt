6ab2e7a5c5d5f70c3fd597f29870d1c2948142c4
Slz <felipegoncalves2311@gmail.com>
2023-02-18 19:27:35 -0300

    More music commands added.

--- src/main/kotlin/michi/bot/commands/music/MusicCommands.kt
+++ src/main/kotlin/michi/bot/commands/music/MusicCommands.kt
@@ -1,6 +1,7 @@
 package michi.bot.commands.music
 
 import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter
+import michi.bot.lavaplayer.PlayerManager
 import michi.bot.util.Emoji
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
@@ -25,6 +26,32 @@ abstract class MusicCommands: AudioEventAdapter() {
 
             audioManager.isSelfDeafened = true
             audioManager.openAudioConnection(channelToJoin)
+        }
+
+        fun play(context: SlashCommandInteractionEvent, url: String) {
+           PlayerManager.instance!!.loadAndPlay(context, url)
+        }
+
+        fun skip(context: SlashCommandInteractionEvent) {
+            val guild = context.guild!!
+            val musicManager = PlayerManager.instance!!.getMusicManager(guild)
+            musicManager.scheduler.nextTrack()
+            context.reply("skiped").queue()
+        }
+
+        fun stop(context: SlashCommandInteractionEvent) {
+            val embed = EmbedBuilder()
+            val musicManager = PlayerManager.instance!!.getMusicManager(context.guild!!)
+            val sender = context.user
+
+            embed.setColor(Color.BLUE)
+                .setTitle("Stoped")
+                .addField("${sender.name} cleared the queue", "No more music? ${Emoji.michiSad}", false)
+            context.replyEmbeds(embed.build())
+                .queue()
+
+            musicManager.scheduler.player.stopTrack()
+            musicManager.scheduler.queue.clear()
         }
 
     }
