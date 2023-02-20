0a6a75f7c0efa26b22d7254dcb0e99ab6c7b23d3
Slz <felipegoncalves2311@gmail.com>
2023-02-20 16:45:23 -0300

    Identation fix

--- src/main/kotlin/michi/bot/commands/math/AnswerLogic.kt
+++ src/main/kotlin/michi/bot/commands/math/AnswerLogic.kt
@@ -9,26 +9,26 @@ import michi.bot.util.Emoji
  * @param mathLogicInstance the user's math problem instance.
  * @author Slz
  */
-     fun checkAnswer(event: MessageReceivedEvent, mathLogicInstance: MathLogic) {
+ fun checkAnswer(event: MessageReceivedEvent, mathLogicInstance: MathLogic) {
 
-        val context: MessageReceivedEvent = event
-        val msg: Int = context.message.contentRaw.toInt()
-        val userName = context.author.name
+    val context: MessageReceivedEvent = event
+    val msg: Int = context.message.contentRaw.toInt()
+    val userName = context.author.name
 
-        // guard clause
-        if (mathLogicInstance.problemInstance.isAnswered || mathLogicInstance.timeEndedUp) return
+    // guard clause
+    if (mathLogicInstance.problemInstance.isAnswered || mathLogicInstance.timeEndedUp) return
 
-        if (msg == mathLogicInstance.problemInstance.result) {
-            val finalTime = (System.currentTimeMillis() - mathLogicInstance.initialTime) / 1000
-            context.channel.sendMessage("**Correct** $userName ${Emoji.michiYesCushion}\nTime: ${finalTime}s")
-                .queue()
-            mathLogicInstance.problemInstance.isAnswered = true
-        }
-
-        else {
-            context.channel.sendMessage("**Wrong** $userName ${Emoji.michiGlare}\nAnswer: ${mathLogicInstance.problemInstance.result}")
-                .queue()
-            mathLogicInstance.problemInstance.isAnswered = true
-        }
-        if(MathLogic.instances.contains(mathLogicInstance)) MathLogic.instances.remove(mathLogicInstance)
+    if (msg == mathLogicInstance.problemInstance.result) {
+        val finalTime = (System.currentTimeMillis() - mathLogicInstance.initialTime) / 1000
+        context.channel.sendMessage("**Correct** $userName ${Emoji.michiYesCushion}\nTime: ${finalTime}s")
+            .queue()
+        mathLogicInstance.problemInstance.isAnswered = true
     }
+
+    else {
+        context.channel.sendMessage("**Wrong** $userName ${Emoji.michiGlare}\nAnswer: ${mathLogicInstance.problemInstance.result}")
+            .queue()
+        mathLogicInstance.problemInstance.isAnswered = true
+    }
+    if(MathLogic.instances.contains(mathLogicInstance)) MathLogic.instances.remove(mathLogicInstance)
+}
