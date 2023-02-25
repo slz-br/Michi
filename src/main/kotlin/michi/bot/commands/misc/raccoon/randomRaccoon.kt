5a399f57d7128f9def604aca57258c1975900c10
Slz <felipegoncalves2311@gmail.com>
2023-02-24 23:29:17 -0300

    randomRaccoon function added

---
+++ src/main/kotlin/michi/bot/commands/misc/raccoon/randomRaccoon.kt
@@ -0,0 +1,22 @@
+package michi.bot.commands.misc.raccoon
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import java.awt.Color
+import java.io.BufferedReader
+import java.io.FileReader
+import kotlin.random.Random
+
+fun randomRaccoon(context: SlashCommandInteractionEvent) {
+    val raccoonImages = BufferedReader(FileReader(".\\txts\\raccoons.txt")).readLines()
+    val imageUrl = raccoonImages[Random.nextInt(raccoonImages.size)]
+
+    val embed = EmbedBuilder()
+    embed.setColor(Color.MAGENTA)
+        if (imageUrl.contains("gif")) { embed.setTitle("Raccoon Gif") }
+        else { embed.setTitle("Raccoon Pic") }
+        .setImage(imageUrl)
+    context.replyEmbeds(embed.build())
+        .setEphemeral(true)
+        .queue()
+}
