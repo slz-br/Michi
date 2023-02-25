3b65fb8f702d55a270ba8b0591ba1696d7885080
Slz <felipegoncalves2311@gmail.com>
2023-02-25 16:15:11 -0300

    Null safety measures

--- src/main/kotlin/michi/bot/commands/misc/wikipedia/randomWiki.kt
+++ src/main/kotlin/michi/bot/commands/misc/wikipedia/randomWiki.kt
@@ -14,7 +14,7 @@ import java.awt.Color
 
 fun randomWiki(context: SlashCommandInteractionEvent) {
     val article = Jsoup.connect("https://en.wikipedia.org//wiki/Special:Random").get()
-    val articleTitle = article.getElementsByTag("h1").first().text()
+    val articleTitle = article.getElementsByTag("h1").first()?.text()
     val articleParagraphs = article.getElementsByTag("p")
     val articleUri = article.baseUri()
     var firstParagraph = "Couldn't load the content ${Emoji.michiSad}"
@@ -29,7 +29,9 @@ fun randomWiki(context: SlashCommandInteractionEvent) {
     val embed = EmbedBuilder()
     embed.setColor(Color.WHITE)
         .setTitle("Wiki", articleUri)
-        .addField(articleTitle, firstParagraph, false)
+        .addField(articleTitle ?: "Title not found ${Emoji.michiTired}", firstParagraph, false)
         .setFooter("Read more at: $articleUri")
-    context.replyEmbeds(embed.build()).queue()
+    context.replyEmbeds(embed.build())
+        .setEphemeral(true)
+        .queue()
 }
