047199415836d1470cd59465071a27ff8c78a0f8
Slz <felipegoncalves2311@gmail.com>
2023-03-01 15:46:10 -0300

    Deprecate message changed

--- src/main/kotlin/michi/bot/commands/CommandManager.kt
+++ src/main/kotlin/michi/bot/commands/CommandManager.kt
@@ -23,9 +23,8 @@ private const val DELAY = 5500L
  * Checks if it is possible to execute a command and manages the users in cooldown.
  * @author Slz
  */
-@Deprecated("This class will be replaced by the SlashCommandManager in the future", level = DeprecationLevel.WARNING)
-abstract class CommandManager {
-    companion object {
+@Deprecated("This class will be replaced by implementations of check function in the commands themselves", level = DeprecationLevel.WARNING)
+object CommandManager {
         private val coolDown = mutableListOf<User>()
 
         /**
