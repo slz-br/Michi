11fddb60d8168eec944a13268a4bb345455f191b
Slz <felipegoncalves2311@gmail.com>
2023-03-01 17:08:03 -0300

    Converted the class to an object and created a check Command(tryToExecute)

--- src/main/kotlin/michi/bot/commands/admin/Ban.kt
+++ src/main/kotlin/michi/bot/commands/admin/Ban.kt
@@ -6,99 +6,144 @@ import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import michi.bot.util.Emoji
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.User
 import java.awt.Color
 import java.util.concurrent.TimeUnit
 
-/**
- * Bans the mentioned member(s) if possible
- * @param context The SlashCommandInteractionEvent that called this function
- * @param reason The reason of the ban.
- * @param subjects The members to ban.
- * @author Slz
- */
+object Ban {
 
-fun ban(context: SlashCommandInteractionEvent, reason: String?, vararg subjects: Member) {
+    /**
+     * Checks if all users to ban are in the guild.
+     * @param context the slashCommandInteractionEvent that called the ban command.
+     * @author Slz
+     */
+    fun tryToExecute(context: SlashCommandInteractionEvent) {
+        val options = context.options
+        val guild = context.guild!!
+        val subjects = mutableListOf<Member>()
+        var reason: String? = null
 
-    // guard clause
-    if (!isPossible(context, subjects.toSet())) return
+        for (option in options) {
+            if (option.type.name != "USER") continue
+            if (option.type.name == "STRING") reason = option.asString
 
-    // if everything is right:
-    val embed = EmbedBuilder()
-    embed.setColor(Color.RED).
-    setTitle("**Ban!** " + Emoji.michiExcite)
+            if (!locateUserInGuild(guild, option.asUser)) {
+                context.reply("couldn't find ${option.asUser.name} in the guild")
+                    .setEphemeral(true)
+                    .queue()
+                return
+            }
 
-    for (subject in subjects.toSet()) {
-        subject.ban(1, TimeUnit.HOURS).queue()
-        embed.addField("Banned ${subject.user.name}", "", false)
+            subjects.add(option.asMember!!)
+        }
+        ban(context, reason, *subjects.toSet().toTypedArray())
     }
 
-    if (reason != null) {
-        embed.addField("Reason: ", "$reason", false)
-    }
+    /**
+     * Bans the mentioned member(s) if possible
+     * @param context The SlashCommandInteractionEvent that called this function
+     * @param reason The reason of the ban.
+     * @param subjects The members to ban.
+     * @author Slz
+     */
+    private fun ban(context: SlashCommandInteractionEvent, reason: String?, vararg subjects: Member) {
 
-    context.replyEmbeds(embed.build())
-        .queue()
-}
+        // guard clause
+        if (!isPossible(context, subjects.toSet())) return
 
-/**
- * Checks if it is possible to ban the subjects
- * @param context The SlashCommandInteraction that called the ban function
- * @param subjects Members to check if they can be banned.
- * @return True if all members can be banned, false if not.
- * @author Slz
- */
+        // if everything is right:
+        val embed = EmbedBuilder()
+        embed.setColor(Color.RED).setTitle("**Ban!** " + Emoji.michiExcite)
 
-private fun isPossible(context: SlashCommandInteractionEvent, subjects: Set<Member>): Boolean {
+        for (subject in subjects.toSet()) {
+            subject.ban(1, TimeUnit.HOURS).queue()
+            embed.addField("Banned ${subject.user.name}", "", false)
+        }
 
-    val agent = context.member!!
-    val agentTopRole = agent.roles.sortedDescending()[0].position
-    val botTopRole = context.guild!!.getMember(context.jda.selfUser)!!.roles.sortedDescending()[0].position
+        if (reason != null) {
+            embed.addField("Reason: ", "$reason", false)
+        }
 
-    // checks if the agent has the permission to ban
-    if (!agent.hasPermission(Permission.BAN_MEMBERS) && !agent.hasPermission(Permission.ADMINISTRATOR)) {
-        context.reply("You don't have the permissions to use this command.")
-            .setEphemeral(true)
+        context.replyEmbeds(embed.build())
             .queue()
-        return false
     }
 
-    // checks if the agent is devil and is trying to ban michi >:(
-    if (subjects.any { it.id == config.get("BOT_ID")}) {
-        context.reply("You can't ban me, idiot ${Emoji.michiUnimpressed}")
-            .setEphemeral(true)
-            .queue()
-        return false
-    }
+    /**
+     * Checks if it is possible to ban the subjects
+     * @param context The SlashCommandInteraction that called the ban function
+     * @param subjects Members to check if they can be banned.
+     * @return True if all members can be banned, false if not.
+     * @author Slz
+     */
+    private fun isPossible(context: SlashCommandInteractionEvent, subjects: Set<Member>): Boolean {
 
-    // checks if the agent is trying to ban himself
-    if (subjects.any { it.user == agent.user }) {
-        context.reply("Are you trying to ban yourself? ${Emoji.michiHuh}")
-            .setEphemeral(true)
-            .queue()
-        return false
-    }
+        val agent = context.member!!
+        val agentTopRole = agent.roles.sortedDescending()[0].position
+        val botTopRole = context.guild!!.getMember(context.jda.selfUser)!!.roles.sortedDescending()[0].position
 
-    for (subject in subjects) {
-
-        // checks if one of the subjects have a role that is hierarchically greater than the agent or the bot
-        if (subject.roles.any { role -> role.position >= agentTopRole || role.position >= botTopRole }) {
-            context.reply("A user that you are trying to ban has a greater role than you or me")
+        // checks if the agent has the permission to ban
+        if (!agent.hasPermission(Permission.BAN_MEMBERS) && !agent.hasPermission(Permission.ADMINISTRATOR)) {
+            context.reply("You don't have the permissions to use this command.")
                 .setEphemeral(true)
                 .queue()
             return false
         }
-        
-        var hadError = false
-        context.guild!!.retrieveBan(subject.user).queue{
-            hadError = true
-            context.reply("A user that you are trying to ban is already banned.")
+
+        // checks if the agent is devil and is trying to ban michi >:(
+        if (subjects.any { it.id == config.get("BOT_ID") }) {
+            context.reply("You can't ban me, idiot ${Emoji.michiUnimpressed}")
                 .setEphemeral(true)
                 .queue()
+            return false
         }
 
-        if (hadError) return false
+        // checks if the agent is trying to ban himself
+        if (subjects.any { it.user == agent.user }) {
+            context.reply("Are you trying to ban yourself? ${Emoji.michiHuh}")
+                .setEphemeral(true)
+                .queue()
+            return false
+        }
 
+        for (subject in subjects) {
+
+            // checks if one of the subjects have a role that is hierarchically greater than the agent or the bot
+            if (subject.roles.any { role -> role.position >= agentTopRole || role.position >= botTopRole }) {
+                context.reply("A user that you are trying to ban has a greater role than you or me")
+                    .setEphemeral(true)
+                    .queue()
+                return false
+            }
+
+            var hadError = false
+            context.guild!!.retrieveBan(subject.user).queue {
+                hadError = true
+                context.reply("A user that you are trying to ban is already banned.")
+                    .setEphemeral(true)
+                    .queue()
+            }
+
+            if (hadError) return false
+
+        }
+
+        return true
     }
 
-    return true
+    /**
+     * Searchs for a member in the guild.
+     * @param guild The guild to look for the user;
+     * @param user The user to search on the guild.
+     * @return True if the user was found in the guild, false if not.
+     * @author Slz
+     */
+    private fun locateUserInGuild(guild: Guild, user: User): Boolean {
+        var userNotFound = false
+
+        guild.retrieveMember(user).queue(null) { userNotFound = true }
+        if (userNotFound) return false
+        return true
+    }
+
 }
--- src/main/kotlin/michi/bot/commands/admin/UnBan.kt
+++ src/main/kotlin/michi/bot/commands/admin/UnBan.kt
@@ -3,88 +3,123 @@ package michi.bot.commands.admin
 import michi.bot.util.Emoji
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import java.awt.Color
 
-/**
- * Unbans the mentioned user(s) if possible
- * @param context The SlashCommandInteractionEvent that called this function.
- * @param subjects The members to unban.
- * @author Slz
- */
-fun unban(context: SlashCommandInteractionEvent, vararg subjects: User) {
+object UnBan {
 
-    // this is asserted because in the SlashCommandListener it's tested if the command
-    // comes or not from a guild
-    val guild = context.guild
+    /**
+     * Checks if the users to unban are actually banned.
+     * @param context The slashCommandInteractionEvent that called the unban command.
+     * @author Slz
+     */
+    fun tryToExecute(context: SlashCommandInteractionEvent) {
+        val options = context.options
+        val guild = context.guild!!
+        val usersToUnban = mutableListOf<User>()
 
-    // guard clauses
-    if (!isPossible(context, subjects.toSet())) return
-    guild ?: return
+        for (subject in options) {
 
-    // if everything is right
-    val embed = EmbedBuilder()
-    embed.setColor(Color.BLUE).
-    setTitle("Unban! ${Emoji.michiHappy}")
+            if (subject.type.name != "USER") continue
 
-    for (subject in subjects) {
-        guild.unban(subject).queue()
-        embed.addField("Unbanned ${subject.name}","", false)
+            if (locateUserInGuild(guild, subject.asUser)) {
+                context.reply("${subject.asUser.name} isn't banned ${Emoji.michiHuh}")
+                    .setEphemeral(true)
+                    .queue()
+                return
+            }
+            usersToUnban.add(subject.asUser)
+        }
+        unban(context, *usersToUnban.toSet().toTypedArray())
     }
 
-    if (subjects.size == 1) {
-        embed.setFooter("It's better that this user don't cause any trouble again")
-        context.replyEmbeds(embed.build())
+    /**
+     * Unbans the mentioned user(s) if possible
+     * @param context The SlashCommandInteractionEvent that called this function.
+     * @param subjects The members to unban.
+     * @author Slz
+     */
+    private fun unban(context: SlashCommandInteractionEvent, vararg subjects: User) {
+
+        // this is asserted because in the SlashCommandListener it's tested if the command
+        // comes or not from a guild
+        val guild = context.guild
+
+        // guard clauses
+        if (!isPossible(context, subjects.toSet())) return
+        guild ?: return
+
+        // if everything is right
+        val embed = EmbedBuilder()
+        embed.setColor(Color.BLUE).setTitle("Unban! ${Emoji.michiHappy}")
+
+        for (subject in subjects) {
+            guild.unban(subject).queue()
+            embed.addField("Unbanned ${subject.name}", "", false)
+        }
+
+        if (subjects.size == 1) {
+            embed.setFooter("It's better that this user don't cause any trouble again")
+            context.replyEmbeds(embed.build())
+        } else {
+            embed.setFooter("It's better that they don't cause any trouble again")
+        }
+
+        context.replyEmbeds(embed.build()).queue()
+
     }
-    else {
-        embed.setFooter("It's better that they don't cause any trouble again")
-    }
 
-    context.replyEmbeds(embed.build()).queue()
+    /**
+     * Checks if it is possible to unban the subjects.
+     * @param context The SlashCommandInteractionEvent that called the unban command.
+     * @param subjects The members to check if they can be unbanned
+     * @return True if all members can be unbanned, false if not.
+     * @author Slz
+     */
 
-}
+    private fun isPossible(context: SlashCommandInteractionEvent, subjects: Set<User>): Boolean {
+        val agent = context.member!!
+        val guild = context.guild!!
+        var userNotBanned = false
 
-/**
- * Checks if it is possible to unban the subjects.
- * @param context The SlashCommandInteractionEvent that called the unban command.
- * @param subjects The members to check if they can be unbanned
- * @return True if all members can be unbanned, false if not.
- * @author Slz
- */
-
-private fun isPossible(context: SlashCommandInteractionEvent, subjects: Set<User>): Boolean {
-    val agent = context.member!!
-    val guild = context.guild!!
-    var userNotBanned = false
-
-    if (subjects.size > 5) {
-        context.reply("You can't unban more than 5 people at the same time with this command.")
-            .setEphemeral(true)
-            .queue()
-        return false
-    }
-
-    for (subject in subjects) {
-
-        // check if the agent has the permissions to use the command
-        if (!agent.hasPermission(Permission.BAN_MEMBERS) && !agent.hasPermission(Permission.ADMINISTRATOR)) {
-            context.reply("You don't have the permissions to use this command.")
+        if (subjects.size > 5) {
+            context.reply("You can't unban more than 5 people at the same time with this command.")
                 .setEphemeral(true)
                 .queue()
             return false
         }
 
-        // checks if the subject is already banned
-        guild.retrieveBan(subject).queue(null) { userNotBanned = true }
-        if (userNotBanned) {
-            context.reply("A user isn't banned")
-                .setEphemeral(true)
-                .queue()
-            return false
+        for (subject in subjects) {
+
+            // check if the agent has the permissions to use the command
+            if (!agent.hasPermission(Permission.BAN_MEMBERS) && !agent.hasPermission(Permission.ADMINISTRATOR)) {
+                context.reply("You don't have the permissions to use this command.")
+                    .setEphemeral(true)
+                    .queue()
+                return false
+            }
+
+            // checks if the subject is already banned
+            guild.retrieveBan(subject).queue(null) { userNotBanned = true }
+            if (userNotBanned) {
+                context.reply("A user isn't banned")
+                    .setEphemeral(true)
+                    .queue()
+                return false
+            }
+
         }
 
+        return true
     }
 
-    return true
+    private fun locateUserInGuild(guild: Guild, user: User): Boolean {
+        var userNotFound = false
+
+        guild.retrieveMember(user).queue(null) { userNotFound = true }
+        if (userNotFound) return false
+        return true
+    }
 }
