9d8d9fa47b7e17f4e1f6bc0a42ea60db424fe512
Slz <felipegoncalves2311@gmail.com>
2023-04-06 20:06:52 -0300

    SlashCommandListener updated: New commands and rewritten commands added.

--- src/main/kotlin/michi/bot/listeners/SlashCommandListener.kt
+++ src/main/kotlin/michi/bot/listeners/SlashCommandListener.kt
@@ -1,9 +1,6 @@
 package michi.bot.listeners
 
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.entities.User
@@ -11,7 +8,8 @@ import java.io.BufferedReader
 import java.io.FileReader
 
 import michi.bot.commands.admin.*
-import michi.bot.commands.math.MathProblem
+import michi.bot.commands.mail.*
+import michi.bot.commands.math.*
 import michi.bot.commands.misc.*
 import michi.bot.commands.util.*
 import michi.bot.util.Emoji
@@ -22,18 +20,18 @@ private const val DELAY = (1000 * 5.5).toLong()
  * Called whenever a slashCommand is used.
  * @author Slz
  */
-
 class SlashCommandListener: ListenerAdapter() {
     companion object {
-        private val cooldownList = mutableSetOf<User>()
 
+        private val cooldownList = mutableSetOf<User>()
         suspend fun cooldownManager(user: User) {
             cooldownList.add(user)
             delay(DELAY)
             cooldownList.remove(user)
         }
+
     }
-    @OptIn(DelicateCoroutinesApi::class)
+
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
         val blackList = BufferedReader(FileReader("BlackList.txt")).readLines()
         val name = event.name
@@ -48,24 +46,37 @@ class SlashCommandListener: ListenerAdapter() {
 
         // Checks if the user is in cooldown
         if (cooldownList.contains(sender)) {
-            event.reply("You are in cooldown, wait a bit ${Emoji.michiSip}")
-                .setEphemeral(true)
-                .queue()
+            event.reply("You are in cooldown, wait a bit ${Emoji.michiSip}").setEphemeral(true).queue()
             return
         }
 
-        // puts the user in cooldown
-        GlobalScope.launch { cooldownManager(sender) }
-
         // if everything is right, try to execute the command
         when (name) {
-            "math"     -> MathProblem.tryToExecute(event)
-            "ban"      -> Ban.tryToExecute(event)
-            "unban"    -> UnBan.tryToExecute(event)
-            "wiki"     -> randomWiki(event)
-            "help"     -> help(event)
-            "raccoon"  -> randomRaccoon(event)
-            "slowmode" -> SlowMode.tryToExecute(event)
+
+            /* Admin Commands */
+            "ban" ->      Ban.execute(event)
+            "unban" ->    UnBan.execute(event)
+            "clear" ->    Clear.execute(event)
+            "slowmode" -> SlowMode.execute(event)
+
+            /* Mail Commands */
+            "mail" ->        Mail.sendMail(event)
+            "mail-window" -> Mail.writeMail(event)
+            "inbox" ->       Mail.inbox(event)
+            "read" ->        Mail.read(event)
+            "remove" ->      Mail.remove(event)
+            "clear-inbox" -> Mail.clearInbox(event)
+            "report-mail" -> Mail.report(event)
+
+
+            /* Misc Commands*/
+            "wiki" ->    Wiki.execute(event)
+            "raccoon" -> Raccoon.execute(event)
+            "math" ->    Math.execute(event)
+
+            /* Util */
+            "help" -> help(event)
+
         }
 
     }
