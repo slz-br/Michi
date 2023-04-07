66017596959f81c668ec420558878286d0bc4b48
Slz <felipegoncalves2311@gmail.com>
2023-04-07 17:45:03 -0300

    new dependencies added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,20 +34,22 @@ repositories {
 }
 
 dependencies {
-    api("org.apache.maven.plugins:maven-compiler-plugin:3.8.1")
-    api("com.fasterxml.jackson.core:jackson-core:2.5.2")
-    api("com.fasterxml.jackson.core:jackson-databind:2.5.2")
-    api("com.fasterxml.jackson.core:jackson-annotations:2.5.2")
-    api("org.apache.maven.plugins:maven-failsafe-plugin:3.0.0-M8")
+    api("org.apache.maven.plugins:maven-compiler-plugin:3.11.0")
+    api("com.fasterxml.jackson.core:jackson-core:2.14.2")
+    api("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    api("com.fasterxml.jackson.core:jackson-annotations:2.14.2")
+    api("org.apache.maven.plugins:maven-failsafe-plugin:3.0.0")
     api("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.20")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.5.0") // get access to kotlin coroutines
-    api("net.dv8tion:JDA:5.0.0-alpha.21") // wrapper for discord api
+    api("net.dv8tion:JDA:5.0.0-beta.2") // wrapper for discord api
     api("io.github.cdimascio:dotenv-java:2.3.1") // api to get configs
-    implementation("ch.qos.logback:logback-classic:1.4.5") // implementation to embellish logs
-    implementation("org.jsoup:jsoup:1.7.2") // api for web scraping
-    testImplementation("org.apache.maven.plugins:maven-surefire-plugin:2.22.2")
+    api("com.github.origma:Google-PerspectiveAPI-Java-Client:0.0.5")
+    implementation("org.yaml:snakeyaml:2.0")
+    implementation("ch.qos.logback:logback-classic:1.4.6") // implementation to embellish logs
+    implementation("org.jsoup:jsoup:1.15.4") // api for web scraping
+    testImplementation("org.apache.maven.plugins:maven-surefire-plugin:3.0.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.7.20")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
 group = "org.example"
