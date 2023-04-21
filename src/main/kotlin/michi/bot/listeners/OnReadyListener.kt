576511f32427bc8d0edc04aa89265abea57b0253
Slz <felipegoncalves2311@gmail.com>
2023-04-21 13:23:27 -0300

    OnReadyListener added.

---
+++ src/main/kotlin/michi/bot/listeners/OnReadyListener.kt
@@ -0,0 +1,34 @@
+package michi.bot.listeners
+
+import michi.bot.Michi.Companion.commandList
+import michi.bot.commands.CommandScope
+import net.dv8tion.jda.api.events.session.ReadyEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.Commands
+
+object OnReadyListener: ListenerAdapter() {
+
+    override fun onReady(event: ReadyEvent) {
+        val commandData: MutableList<CommandData> = ArrayList()
+
+        commandList.forEach { cmd ->
+            if (cmd.scope == CommandScope.GLOBAL_SCOPE) {
+                val command = Commands.slash(cmd.name.lowercase(), cmd.description)
+                for (arg in cmd.arguments) {
+                    command.addOption(
+                        arg.type,
+                        arg.name.lowercase(),
+                        arg.description,
+                        arg.isRequired,
+                        arg.hasAutoCompletion
+                    )
+                }
+
+                commandData.add(command)
+            }
+        }
+
+        event.jda.updateCommands().addCommands(commandData).queue()
+    }
+}
