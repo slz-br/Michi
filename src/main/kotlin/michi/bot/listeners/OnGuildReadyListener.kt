3e65038b37fe9d7f94037fed09295036a1641b66
Slz <felipegoncalves2311@gmail.com>
2023-07-08 18:47:30 -0300

    OnGuildReadyListener updated: - Hardcoded commands removed. - Safety measures(Michi doesn't stay in NSFW guilds)

--- src/main/kotlin/michi/bot/listeners/OnGuildReadyListener.kt
+++ src/main/kotlin/michi/bot/listeners/OnGuildReadyListener.kt
@@ -9,7 +9,6 @@ import michi.bot.lavaplayer.PlayerManager
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 
@@ -26,61 +25,36 @@ object OnGuildReadyListener: ListenerAdapter() {
             GuildsDAO.post(guild)
             PlayerManager.retrieveGuildMusicQueue(guild)
 
-        // help
-        commandData.add(Commands.slash("help", "Relevant info about michi."))
-
-        // mail
-        commandData.add(Commands.slash("mail", "Send an anonymous message to an user.")
-            .addOption(OptionType.STRING, "title", "What's the mail about?", true)
-            .addOption(OptionType.STRING, "message", "The content/body of the mail.", true)
-            .addOption(OptionType.USER, "receiver", "Who are you sending this mail to?", true)
-        )
-
-        // mail-window
-        commandData.add(Commands.slash("mail-window", "Send an anonymous message to an user but using a window."))
-
-        // inbox
-        commandData.add(Commands.slash("inbox", "Check the mails you received.")
-            .addOption(OptionType.INTEGER, "page", "The inbox page you want to see.", true)
-        )
-
-        // read
-        commandData.add(Commands.slash("read", "Read an mail from your inbox.")
-            .addOption(OptionType.INTEGER, "position", "The position of the email you want to read in your inbox.", true)
-        )
-
-        // remove
-        commandData.add(Commands.slash("remove", "Remove an email from your inbox.")
-            .addOption(OptionType.INTEGER, "position", "The position of the mail you want to remove in your inbox.", true)
-        )
-
-        Michi.commandList.forEach { cmd ->
-
-            if (cmd.scope == CommandScope.GUILD_SCOPE) {
-
-                val command = Commands.slash(cmd.name.lowercase(), cmd.description)
-                for (arg in cmd.arguments) {
-                    command.addOption(
-                        arg.type,
-                        arg.name.lowercase(),
-                        arg.description,
-                        arg.isRequired,
-                        arg.hasAutoCompletion
-                    )
-                }
-
-                commandData.add(command)
+            if (guild.nsfwLevel == Guild.NSFWLevel.EXPLICIT || guild.nsfwLevel == Guild.NSFWLevel.AGE_RESTRICTED) {
+                BlacklistDAO.post(guild = guild, reason = "Discord tagged server as NSFW.")
+                guild.leave().queue()
+                return@launch
             }
+
+            // Registering guild commands
+            val commandData = mutableSetOf<CommandData>()
+
+            Michi.commandList.forEach { cmd ->
+
+                if (cmd.scope == CommandScope.GUILD_SCOPE) {
+
+                    val command = Commands.slash(cmd.name.lowercase(), cmd.description)
+                    for (arg in cmd.arguments) {
+                        command.addOption(
+                            arg.type,
+                            arg.name.lowercase(),
+                            arg.description,
+                            arg.isRequired,
+                            arg.hasAutoCompletion
+                        )
+                    }
+                    commandData.add(command)
+
+                }
+            }
+            guild.updateCommands().addCommands(commandData).queue()
+
         }
-
-        // report
-        commandData.add(Commands.slash("report-mail", "Reports a mail that was sent to you.")
-            .addOption(OptionType.INTEGER, "mail-position", "The position of the mail in your inbox.")
-        )
-
-        // clearInbox
-        commandData.add(Commands.slash("clear-inbox", "Clears your mails inbox."))
-
-        event.guild.updateCommands().addCommands(commandData).queue()
     }
+
 }
