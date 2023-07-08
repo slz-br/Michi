36c92d26771d50aa21b9781a943863e3dc35e049
Slz <felipegoncalves2311@gmail.com>
2023-07-08 18:22:07 -0300

    Indentation fix and new emoji

--- src/main/kotlin/michi/bot/util/Emoji.kt
+++ src/main/kotlin/michi/bot/util/Emoji.kt
@@ -6,34 +6,35 @@ package michi.bot.util
  */
 object Emoji {
 
-        // All the emojis are from these BNA servers:
-        // https://discord.gg/bna
-        // https://discord.gg/zGUJwG3
+    // All the emojis are from these BNA servers:
+    // https://discord.gg/bna
+    // https://discord.gg/zGUJwG3
 
-        const val michiUnimpressed = "<:michiUnimpressed:1071852149283225690>"
-        const val smolMichiAngry = "<:smolMichiAngry:1071851332606107658>"
-        const val michiExcite = "<:michiExcite:1071851727646634096>"
-        const val michiHuh = "<:michiHuh:1071853598847283200>"
-        const val michiYesCushion = "<:michiYesCushion:1072188761498189935>"
-        const val michiThink = "<:michiThink:1072444573206450256>"
-        const val michiGlare = "<:michiGlare:1072451435427549245>"
-        const val michiHappy = "<:michiHappy:1072927274527096992>"
-        const val michiTroll = "<:michiTroll:1072944747188060240>"
-        const val michiBlep = "<:michiBlep:1073468499910991873>"
-        const val michiMusic = "<:michiMusic:1073474325002334208>"
-        const val michiSad = "<:michiSad:1073476670482288692>"
-        const val michiSmug = "<:michiSmug:1076637043888758895>"
-        const val michiTired = "<:michiTired:1077706713274257562>"
-        const val michiSip = "<:michiSip:1079562183131807794>"
-        const val michiOpsie = "<:michiOpsie:1083321811036540988>"
-        const val michiShrug = "<:michiShrug:1083994089537683537>"
-        const val michiJoy = "<:michiJoy:1085382627034402878>"
-        const val michiHMPH = "<:michiHMPH:1085491176406265886>"
-        const val michiThumbsUp = "<:michiThumbsUp:1087926139382681620>"
+    const val michiUnimpressed = "<:michiUnimpressed:1071852149283225690>"
+    const val smolMichiAngry = "<:smolMichiAngry:1071851332606107658>"
+    const val michiExcite = "<:michiExcite:1071851727646634096>"
+    const val michiHuh = "<:michiHuh:1071853598847283200>"
+    const val michiYesCushion = "<:michiYesCushion:1072188761498189935>"
+    const val michiThink = "<:michiThink:1072444573206450256>"
+    const val michiGlare = "<:michiGlare:1072451435427549245>"
+    const val michiHappy = "<:michiHappy:1072927274527096992>"
+    const val michiTroll = "<:michiTroll:1072944747188060240>"
+    const val michiBlep = "<:michiBlep:1073468499910991873>"
+    const val michiMusic = "<:michiMusic:1073474325002334208>"
+    const val michiSad = "<:michiSad:1073476670482288692>"
+    const val michiSmug = "<:michiSmug:1076637043888758895>"
+    const val michiTired = "<:michiTired:1077706713274257562>"
+    const val michiSip = "<:michiSip:1079562183131807794>"
+    const val michiOpsie = "<:michiOpsie:1083321811036540988>"
+    const val michiShrug = "<:michiShrug:1083994089537683537>"
+    const val michiJoy = "<:michiJoy:1085382627034402878>"
+    const val michiHMPH = "<:michiHMPH:1085491176406265886>"
+    const val michiThumbsUp = "<:michiThumbsUp:1087926139382681620>"
+    const val michiLook = "<:michiLook:1106051696376946798>"
 
-        // animated emojis
-        const val michiNodders = "<a:michiNodders:1073854963555516416>"
-        const val michiSaddened = "<a:michiSaddened:1078247891216367626>"
-        const val nightRunning = "<a:nightRunning:1078601671233511525>"
+    // animated emojis
+    const val michiNodders = "<a:michiNodders:1073854963555516416>"
+    const val michiSaddened = "<a:michiSaddened:1078247891216367626>"
+    const val nightRunning = "<a:nightRunning:1078601671233511525>"
 
 }
