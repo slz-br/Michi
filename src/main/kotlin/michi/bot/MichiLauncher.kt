a7d03b9aea8684ed5a86762a11888a59aa25138d
Slz <felipegoncalves2311@gmail.com>
2024-01-18 18:47:36 -0300

    Updated MichiLauncher: - Put some things as static

--- src/main/kotlin/michi/bot/MichiLauncher.kt
+++ src/main/kotlin/michi/bot/MichiLauncher.kt
@@ -1,9 +1,9 @@
 package michi.bot
 
-import au.com.origma.perspectiveapi.v1alpha1.PerspectiveAPI
 import ch.qos.logback.core.status.Status
 import io.github.cdimascio.dotenv.Dotenv
 import net.dv8tion.jda.api.utils.cache.CacheFlag
+import net.dv8tion.jda.api.utils.MemberCachePolicy
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder
@@ -18,17 +18,24 @@ import michi.bot.commands.CommandDeactivated
 import michi.bot.listeners.*
 import michi.bot.database.DataBaseFactory
 import michi.bot.commands.MichiCommand
-import net.dv8tion.jda.api.utils.MemberCachePolicy
 
+/**
+ * Holder of the environment variables.
+ * please check the .env file or create it if you haven't yet.
+ *
+ * Usage: config[["KEY"]]
+ *
+ * Example: config[["TOKEN"]] // to retrieve the Token from the .env file.
+ */
 val config: Dotenv = Dotenv.configure().load()
-val perspectiveAPI: PerspectiveAPI = PerspectiveAPI.create(config["PERSPECTIVE_API_TOKEN"])
 
-// todo: Implement the SetDj command
 // todo: migrate to LavaLink.kt -> do after release
 // todo: support for auto registering context commands -> do after release
-// todo: store mail inbox in the database
 // todo: document every command in each language. Do this by creating a md file for every language and update it with the info gathered from the commands.
-// todo: table for users -> send dm responses in the user's preferred language
+
+// do asap
+// todo: store mail inbox in the database
+// todo: store the guild type racer leaderboard, so Michi doesn't have to search the whole db again everytime.
 
 private val currentTime: LocalDateTime
     get() = LocalDateTime.now()
@@ -40,10 +47,10 @@ private val currentTime: LocalDateTime
 suspend fun main() {
     println(currentTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm.SSS")))
     when(currentTime.hour) {
-        in 0  until 6 -> println("Relaxing in the cozy dawn, aren't you?")
-        in 6  until 12 -> println("Good morning! Want some coffee?")
-        in 12 until 18 -> println("Good afternoon! Don't forget to touch some grass.")
-        in 18 until 24 -> println("Good evening/night! Are you listening to Night Running?")
+        in 0 ..< 6 -> println("Relaxing in the cozy dawn, aren't you?")
+        in 6 ..< 12 -> println("Good morning! Do you want some coffee?")
+        in 12 ..< 18 -> println("Good afternoon! Don't forget to touch some grass.")
+        in 18 ..< 24 -> println("Good evening/night! Are you listening to Night Running?")
     }
 
     println(
@@ -52,7 +59,7 @@ suspend fun main() {
             |  \/  |_ _/ __| || |_ _|
             | |\/| || | (__| __ || | 
             |_|  |_|___\___|_||_|___|
-            
+
         """.trimIndent()
     )
 
@@ -70,8 +77,79 @@ suspend fun main() {
 class Michi {
 
     companion object {
+
+        /**
+         * List containing all Michi command's. The commands are added at run time to the commandList via reflection.
+         * All classes that extend [michi.bot.commands.MichiCommand] except the ones annotated with [michi.bot.commands.CommandDeactivated]
+         * @see registerCommands
+         */
         val commandList = mutableListOf<MichiCommand>()
-        val logger = LoggerFactory.getLogger(Michi::class.java)
+
+        /**
+         * Logger for the Michi class. Used to print information about the config files and if the commands were properly
+         * registered.
+         */
+        val logger: Logger = LoggerFactory.getLogger(Michi::class.java)
+
+        /**
+         * Function used to register Michi's commands. It looks up for all the classes within the subdirectories of [michi.bot.commands]
+         * that extend [michi.bot.commands.MichiCommand] and aren't annotated with [michi.bot.commands.CommandDeactivated].
+         * @see searchCommandDirectories
+         */
+
+        private fun registerCommands() {
+            val commandsDir = File("${System.getProperty("user.dir")}\\build\\classes\\kotlin\\main\\michi\\bot\\commands")
+            val allCommandsDir = searchCommandDirectories(commandsDir)
+
+            allCommandsDir.forEach { currentDir -> // loop entre todas as pastas de comandos
+                currentDir.listFiles()?.forEach fileLoop@{ file -> // loop entre todos os arquivos da pasta de comandos
+                    if (!file.exists() || !file.name.endsWith(".class") || file.name.contains('$')) return@fileLoop
+
+                    val packageName = currentDir.canonicalPath.split("main\\")[1]
+
+                    val qualifiedName = "$packageName${file.canonicalPath.removePrefix(currentDir.canonicalPath)}"
+                        .dropLast(6)
+                        .replace('\\', '.')
+
+                    val commandClass = Class.forName(qualifiedName)
+
+                    if (
+                        !MichiCommand::class.java.isAssignableFrom(commandClass)
+                        || commandClass.isAnnotationPresent(CommandDeactivated::class.java)
+                    ) return@fileLoop
+
+                    val cmdConstructor = commandClass.asSubclass(MichiCommand::class.java).getDeclaredConstructor()
+                    cmdConstructor.isAccessible = true
+                    commandList += cmdConstructor.newInstance()
+                }
+            }
+            logger.info("Commands loaded | Amount: ${commandList.size}")
+        }
+
+        /**
+         * Function that uses the breadth first search algorithm to look up for all the directories within
+         * the [michi.bot.commands] directory.
+         */
+        private fun searchCommandDirectories(initialDir: File): Set<File> {
+            if (!initialDir.isDirectory || !initialDir.exists()) return emptySet()
+
+            val commandDirectoriesList = mutableSetOf<File>()
+
+            val visited = mutableSetOf(initialDir)
+            val deque = LinkedBlockingDeque<File>().apply { add(initialDir) }
+
+            while (deque.isNotEmpty()) {
+                val dir = deque.pop()
+                dir.listFiles()!!.forEach {
+                    if (!it.isDirectory || !it.exists() || it in visited) return@forEach
+                    commandDirectoriesList += it
+                    visited += it
+                    deque += it
+                }
+            }
+
+            return commandDirectoriesList
+        }
     }
 
     init {
@@ -82,7 +160,7 @@ class Michi {
 
         if (!configFile.exists()) {
             configFile.createNewFile()
-            logger.warn("Looks like you are trying to boot Michi for the first time.\n You must configure her in the file \".env\"\nFollow the example in the file \"example.env\"")
+            logger.warn("Looks like you are trying to boot Michi for the first time.\nYou must configure her in the file \".env\"\nFollow the example in the file \"example.env\"")
             exitProcess(Status.INFO)
         }
 
@@ -112,8 +190,11 @@ class Michi {
             // Gateway intents
             enableIntents(
                 GatewayIntent.MESSAGE_CONTENT,
-                GatewayIntent.GUILD_VOICE_STATES
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.GUILD_MEMBERS
             )
+
+            setMemberCachePolicy(MemberCachePolicy.ALL)
 
             // Enable cache
             enableCache(CacheFlag.VOICE_STATE)
@@ -121,56 +202,6 @@ class Michi {
         }.build()
 
         registerCommands()
-    }
-
-    private fun registerCommands() {
-        val commandsDir = File("${System.getProperty("user.dir")}\\build\\classes\\kotlin\\main\\michi\\bot\\commands")
-        val allCommandsDir = searchCommandDirectories(commandsDir)
-
-        allCommandsDir.forEach { currentDir ->
-            currentDir.listFiles()?.forEach fileLoop@{ file ->
-                if (!file.exists() || !file.name.endsWith(".class") || file.name.contains('$')) return@fileLoop
-
-                val packageName = currentDir.canonicalPath.split("main\\")[1]
-
-                val qualifiedName = "$packageName${file.canonicalPath.removePrefix(currentDir.canonicalPath)}"
-                    .dropLast(6)
-                    .replace('\\', '.')
-
-                val commandClass = Class.forName(qualifiedName)
-
-                if (
-                    !MichiCommand::class.java.isAssignableFrom(commandClass)
-                    || commandClass.isAnnotationPresent(CommandDeactivated::class.java)
-                ) return@fileLoop
-
-                val cmdConstructor = commandClass.asSubclass(MichiCommand::class.java).getDeclaredConstructor()
-                cmdConstructor.isAccessible = true
-                commandList.add(cmdConstructor.newInstance())
-            }
-        }
-        logger.info("Commands loaded | Amount: ${commandList.size}")
-    }
-
-    private fun searchCommandDirectories(initialDir: File): Set<File> {
-        if (!initialDir.isDirectory || !initialDir.exists()) return emptySet()
-
-        val commandDirectoriesList = mutableSetOf<File>()
-
-        val visited = mutableSetOf(initialDir)
-        val deque = LinkedBlockingDeque<File>().apply { add(initialDir) }
-
-        while (deque.isNotEmpty()) {
-            val dir = deque.pop()
-            dir.listFiles()!!.forEach {
-                if (!it.isDirectory || !it.exists() || it in visited) return@forEach
-                commandDirectoriesList.add(it)
-                visited.add(it)
-                deque.add(it)
-            }
-        }
-
-        return commandDirectoriesList
     }
 
 }
